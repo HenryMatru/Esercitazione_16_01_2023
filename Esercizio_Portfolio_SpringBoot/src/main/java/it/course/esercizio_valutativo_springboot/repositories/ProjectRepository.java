cac4e99801959362b66d2e89699f6987edff9918
Enrico Matrullo <80920594+HenryMatru@users.noreply.github.com>
2023-01-16 21:45:44 +0100

    Delete Esercizio_Portfolio_SpringBoot directory

--- Esercizio_Portfolio_SpringBoot/src/main/java/it/course/esercizio_valutativo_springboot/repositories/ProjectRepository.java
+++
@@ -1,7 +0,0 @@
-package it.course.esercizio_valutativo_springboot.repositories;
-
-import it.course.esercizio_valutativo_springboot.models.Project;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProjectRepository extends JpaRepository<Project, Long> {
-}
--- Esercizio_Portfolio_SpringBoot/src/main/java/it/course/esercizio_valutativo_springboot/repositories/SkillRepository.java
+++
@@ -1,7 +0,0 @@
-package it.course.esercizio_valutativo_springboot.repositories;
-
-import it.course.esercizio_valutativo_springboot.models.Skill;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SkillRepository extends JpaRepository<Skill, Long> {
-}
